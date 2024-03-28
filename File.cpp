86402618ce9efff1475a1c84db68f737fb2e45da
Renata Espinosa <114301354+Renata1214@users.noreply.github.com>
2024-03-28 12:25:24 +0000

    Changes

--- File.cpp
+++ File.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include "mylist.h"
+#include <string>
+
+using namespace std;
+
+
+int main (){
+
+    List s;
+    s.push_back("apple");
+    s.push_back("pear");
+    s.push_back("banana");
+
+     s.print();
+
+     cout << endl;
+
+    s.swap_nodes();
+
+    s.print();
+
+
+    return 0;
+}
+
+
+
 
---
+++ mylist.h
@@ -0,0 +1,50 @@
+#include "payload.h"
+#include <iostream>
+
+using std::string;
+
+struct Node
+{
+//Members of the structure
+    Payload instance;
+    Node *next_link;
+//Constructor
+    Node (Payload n);
+};
+
+struct List
+{
+// Members of list
+    Node *Head_List; 
+//Constructor
+List ();
+//Destructor
+~List();
+//Deleting and adding Nodes to list
+void push_front (const Payload& name);
+void pop_front ();
+void push_back (const Payload& name);
+void pop_back ();
+//Size
+int size()const;
+
+
+// CAPACITY 
+int capacity ()const;
+
+//Print
+void print () const;
+
+Payload& operator[](int i);
+
+Payload& operator[](int i) const;
+
+//Additional Functions 
+bool empty() const; 
+Payload front() const; 
+Payload back() const;
+
+Node* swap_nodes () ;
+
+
+};
